f68f3517cae9f7f9bb5fbceee5749f749f94bd98
Felipe Perez <perezmaf@myumanitoba.ca>
2024-10-10 06:36:24 -0500

    Revert "Fix experiments config parsing"
    This reverts commit 3b10dfd5479a06845f5fb9e258d4a6e1dda1807d.

--- train.py
+++ train.py
@@ -65,10 +65,7 @@ train_trans = choose_training_augmentations(exp_config)
 val_trans = get_validation_augmentations(*exp_config['data']['val']['normalization'])
 
 ###########  GET OTHER DATA ##############
-if os.path.exists("../data/DATASET_DEF1_METADATA_train.json") and os.path.exists("../data/DATASET_DEF1_METADATA_test.json"):
-    geo_data = get_geo_data("../data/DATASET_DEF1_METADATA_train.json", "../data/DATASET_DEF1_METADATA_test.json")
-else:
-    geo_data = None
+geo_data = get_geo_data("../data/DATASET_DEF1_METADATA_train.json", "../data/DATASET_DEF1_METADATA_test.json")
 metadata = exp_config["metadata"]
 
 ###########   DATAMODULE    ##########
